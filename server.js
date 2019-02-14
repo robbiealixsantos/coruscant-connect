a0aa02717f8cb2c061a828e7cc6801c2ea641f42
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2019-02-14 07:16:38 -0800

    add register user route

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profiles = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connected to MongoDB
-mongoose.connect(db)
-    .then(() => console.log('Mongo.DB Connected'))
-    .catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('testing'));
+// Passport middleware
+app.use(passport.initialize());
 
 // Use Routes
-
 app.use('/api/users', users);
-app.use('/api/profiles', profiles);
 app.use('/api/posts', posts);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port , () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
