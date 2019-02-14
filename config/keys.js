9a804541daebae0a785690105885fe1d03542002
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2019-02-13 19:20:40 -0800

    mongoose and mongo  initialization

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Robbie:ftrobbie@coruscant-connect-shard-00-00-q1nau.mongodb.net:27017,coruscant-connect-shard-00-01-q1nau.mongodb.net:27017,coruscant-connect-shard-00-02-q1nau.mongodb.net:27017/test?ssl=true&replicaSet=coruscant-connect-shard-0&authSource=admin&retryWrites=true'
+};
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connected to MongoDB
+mongoose.connect(db).then(() => console.log('Mongo.DB Connected')).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello'));
 
