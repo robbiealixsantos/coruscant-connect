6615d3d76e43f94a16a0bb3110f61c5a3756afe5
Robbie <robbiealixsantos@gmail.com>
2019-02-17 05:05:27 -0800

    Add react client and initial landing page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/layout/NavBar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
