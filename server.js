8241e500764c40af923d4578587b5e2183ae1210
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2019-02-20 09:28:48 -0800

    heroku deploy prep

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -34,6 +35,16 @@ require ('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profiles);
 app.use('/api/post', posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
